a16f03bad5eb9a4e15bb38edc6441e0f6ea0bf86
Lucas Silva <l.silva8692@gmail.com>
2023-08-20 18:13:04 -0300

    add footer

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,13 @@
+import Footer from "../Footer";
 import Navbar from "../Navbar";
 import styles from "./main.module.css";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </>
   );
 };
